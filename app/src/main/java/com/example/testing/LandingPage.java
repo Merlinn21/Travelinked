4bea9c99fed13fce1e0c975962283eb813ebc312
RAFLY <anandarafly15@gmail.com>
2020-06-12 20:32:01 +0700

    landing page pas di klik bali ke guide profile

--- app/src/main/java/com/example/testing/LandingPage.java
+++ app/src/main/java/com/example/testing/LandingPage.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +18,7 @@ public class LandingPage extends AppCompatActivity {
     TextView pack;
     FirebaseUser user;
     FirebaseAuth mAuth;
+    ImageView bali;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,14 @@ public class LandingPage extends AppCompatActivity {
         pack = findViewById(R.id.textBag);
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
+        bali = findViewById(R.id.imageBali);
+
+        bali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LandingPage.this,guidePage.class));
+            }
+        });
 
     }
     private void ToastMaker(String text){
